438a8335c9f1a8671b7c8fe5727e528cc50b1e7b
Sean Murphy <sean@wordfence.com>
2017-05-05 11:41:26 -0400

    Allow disabled attribute

--- addon/components/time-field.js
+++ addon/components/time-field.js
@@ -23,7 +23,7 @@ export default Component.extend({
   tagName: 'input',
   type: 'text',
   autocomplete: false,
-  attributeBindings: ['type', 'placeholder', 'name', 'autocomplete'],
+  attributeBindings: ['type', 'placeholder', 'name', 'autocomplete', 'disabled'],
 
   hour12: false,
 
